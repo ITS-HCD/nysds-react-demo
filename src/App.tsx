41392918cdd59e331fddbb762cc6c6c6cd28d863
Robert Chen <bergintonatcapital@gmail.com>
2025-04-17 17:00:19 -0400

    Reorganize react demo imports, add unav and global header/footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useState } from 'react';
 import './App.css';
-import { Alert, TextInput, Textarea, Select, RadioGroup, Radiobutton, Checkbox, Checkboxgroup, Toggle, Button, Option } from "./utils/nysds-components";
+import { NysAlertComponent, NysButtonComponent, NysCheckboxComponent, NysCheckboxgroupComponent, NysGlobalFooterComponent, NysGlobalHeaderComponent, NysOptionComponent, NysRadiobuttonComponent, NysRadioGroupComponent, NysSelectComponent, NysTextInputComponent, NysTextareaComponent, NysToggleComponent, NysUnavFooterComponent, NysUnavHeaderComponent} from "./utils/nysds-components";
 
 
 function App() {
@@ -24,91 +24,108 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React + NYSDS</h1>
-      <Alert 
-        type="info"
-        heading="NYSDS Components"
-        text="We make use of form-related NYSDS components in the native HTML form below."
-        primaryLabel="Learn more on NYSDS reference site"
-        primaryAction="https://designsystem.ny.gov/"
-        dismissible
-      />
-
-      <form
-        id="myForm"
-        className="nys-newsletter-form"
-        method="POST"
-        onSubmit={handleSubmit}
-      >
-        <TextInput name='fullName' label="Full name" required/>
-        <TextInput name='email' label="Email" type="email" required/>
-        <Textarea name='quote' label="Enter your favorite quote:" value="Majorities, of course, start with minorities." required/>
-        
-        <Select name="newsletter_topic" label="Select your preferred newsletter topic" id="newsletter-topic" required>
-          <Option value="government_updates" label="Government Updates" />
-          <Option value="community_events" label="Community Events" />
-          <Option value="public_services" label="Public Services & Resources" />
-          <Option value="transportation_news" label="Transportation & Infrastructure" />
-          <Option value="environment" label="Environment & Sustainability" />      
-        </Select>
-
-        <RadioGroup
-          label="How often do you want updates?"
-          description="These updates will be sent directly to your email."
-          size="md"
-          required
-        >
-          <Radiobutton
-            name="frequency"
-            label="Weekly"
-            value="weekly"
-          />
-          <Radiobutton
-            name="frequency"
-            label="Monthly"
-            value="monthly"
-          />
-        </RadioGroup>
-
-        <Checkbox
-          label="Subscribe to NYS Government Updates"
-          description="Get notified via email about important updates and services."
-          id="subscribe-checkbox-disabled-checked"
-          name="subscribe"
-          value="email-updates"
-          required
-        />
-
-        <Checkboxgroup
-          label="Select your favorite New York landmarks"
-          description="Choose from the options below"
-          required
-        >
-          <Checkbox name="landmarks" label="Adirondacks" value="adirondacks" />
-          <Checkbox name="landmarks" value="finger-lakes" label="Finger Lakes" />
-          <Checkbox name="landmarks" value="catskills" label="Catskills" />
-          <Checkbox name="landmarks" value="niagara-falls" label="Niagara Falls"/>
-          <Checkbox name="landmarks" value="coney-island" label="Coney Island"/>
-        </Checkboxgroup>
-        <Toggle
-          label="Dark Mode"
-          name="toggle-switch"
-          value="access"><p slot="description">Toggle switch is usually NOT recommended for forms (use checkboxes instead).</p></Toggle>
-        <Button
-          type="submit"
-          fullWidth
-          label="Subscribe"
-        />
-      </form>
-      <br></br>
-      {submittedData && (
-        <Alert
-          type="success"
-          heading="Form submitted successfully"
-          text={`${submittedData ? "\n\nSubmitted Data:\n" + JSON.stringify(submittedData, null, 2) : ""}`}
+      <NysUnavHeaderComponent />
+      <NysGlobalHeaderComponent appName="React Demo Form" agencyName="Office of Information Technology Services" />
+      
+      <main>
+        <h1 className='appTitle'>Vite + React + NYSDS</h1>
+        <NysAlertComponent 
+          type="info"
+          heading="NYSDS Components"
+          text="We make use of form-related NYSDS components in the native HTML form below."
+          primaryLabel="Learn more on NYSDS reference site"
+          primaryAction="https://designsystem.ny.gov/"
           dismissible
         />
-      )}
+
+        <form
+          id="myForm"
+          className="nys-newsletter-form"
+          method="POST"
+          onSubmit={handleSubmit}
+        >
+          <NysTextInputComponent name='fullName' label="Full name" required/>
+          <NysTextInputComponent name='email' label="Email" type="email" required/>
+          <NysTextareaComponent name='quote' label="Enter your favorite quote:" value="Majorities, of course, start with minorities." required/>
+          
+          {/* Note: when wrapping components within a component, you will need a closing tag (e.g. select, radiogroup, checkboxgroup, slot wrappings for inner HTML elements) */}
+          <NysSelectComponent name="newsletter_topic" label="Select your preferred newsletter topic" id="newsletter-topic" required>
+            <NysOptionComponent value="government_updates" label="Government Updates" />
+            <NysOptionComponent value="community_events" label="Community Events" />
+            <NysOptionComponent value="public_services" label="Public Services & Resources" />
+            <NysOptionComponent value="transportation_news" label="Transportation & Infrastructure" />
+            <NysOptionComponent value="environment" label="Environment & Sustainability" />      
+          </NysSelectComponent>
+
+          <NysRadioGroupComponent
+            label="How often do you want updates?"
+            description="These updates will be sent directly to your email."
+            size="md"
+            required
+          >
+            <NysRadiobuttonComponent
+              name="frequency"
+              label="Weekly"
+              value="weekly"
+            />
+            <NysRadiobuttonComponent
+              name="frequency"
+              label="Monthly"
+              value="monthly"
+            />
+          </NysRadioGroupComponent>
+
+          <NysCheckboxComponent
+            label="Subscribe to NYS Government Updates"
+            description="Get notified via email about important updates and services."
+            id="subscribe-checkbox-disabled-checked"
+            name="subscribe"
+            value="email-updates"
+            required
+          />
+
+          <NysCheckboxgroupComponent
+            label="Select your favorite New York landmarks"
+            description="Choose from the options below"
+            required
+          >
+            <NysCheckboxComponent name="landmarks" label="Adirondacks" value="adirondacks" />
+            <NysCheckboxComponent name="landmarks" value="finger-lakes" label="Finger Lakes" />
+            <NysCheckboxComponent name="landmarks" value="catskills" label="Catskills" />
+            <NysCheckboxComponent name="landmarks" value="niagara-falls" label="Niagara Falls"/>
+            <NysCheckboxComponent name="landmarks" value="coney-island" label="Coney Island"/>
+          </NysCheckboxgroupComponent>
+
+          <NysToggleComponent
+            label="Dark Mode"
+            name="toggle-switch"
+            value="access"><p slot="description">Toggle switch is usually NOT recommended for forms (use checkboxes instead).</p>
+          </NysToggleComponent>
+
+          <NysButtonComponent
+            type="submit"
+            fullWidth
+            label="Subscribe"
+          />
+        </form>
+        <br></br>
+        {submittedData && (
+          <NysAlertComponent
+            type="success"
+            heading="Form submitted successfully"
+            text={`${submittedData ? "\n\nSubmitted Data:\n" + JSON.stringify(submittedData, null, 2) : ""}`}
+            dismissible
+          />
+        )}
+      </main>
+
+      <NysGlobalFooterComponent agencyName="Office of Information Technology Services">
+        <ul>
+          <li><a href="https://its.ny.gov">ITS Home</a></li>
+          <li><a href="https://its.ny.gov/about">About ITS</a></li>
+        </ul>
+      </NysGlobalFooterComponent>
+      <NysUnavFooterComponent />
     </>
   );
 }
--- src/utils/nysds-components.ts
+++ src/utils/nysds-components.ts
@@ -1,11 +1,13 @@
 import { createComponent } from "@lit/react";
 
-// Import the components so that the custom elements are registered
+// Import the components so that the custom elements are registered (below are some examples we will use in this demo)
 import {
    NysAlert,
    NysButton,
    NysCheckbox,
    NysCheckboxgroup,
+   NysGlobalFooter,
+   NysGlobalHeader,
    NysOption,
    NysRadiobutton,
    NysRadiogroup,
@@ -13,73 +15,100 @@ import {
    NysTextarea,
    NysTextinput,
    NysToggle,
-
+	NysUnavFooter,
+   NysUnavHeader,
 } from "@nysds/components";
 import * as React from "react";
 
 /***** Further instruction on why we do this can be found on: https://lit.dev/docs/frameworks/react/ *****/
 // Wrap components
-export const TextInput = createComponent({
-	react: React,
-	tagName: "nys-textinput",
-	elementClass: NysTextinput,
-});
-
-export const Textarea = createComponent({
-	react: React,
-	tagName: "nys-textarea",
-	elementClass: NysTextarea,
-});
-
-export const Checkbox = createComponent({
-	react: React,
-	tagName: "nys-checkbox",
-	elementClass: NysCheckbox,
-});
-
-export const Checkboxgroup = createComponent({
-	react: React,
-	tagName: "nys-checkboxgroup",
-	elementClass: NysCheckboxgroup,
-});
-
-export const Radiobutton = createComponent({
-	react: React,
-	tagName: "nys-radiobutton",
-	elementClass: NysRadiobutton,
-});
-
-export const RadioGroup = createComponent({
-	react: React,
-	tagName: "nys-radiogroup",
-	elementClass: NysRadiogroup,
-});
-
-export const Select = createComponent({
-	react: React,
-	tagName: "nys-select",
-	elementClass: NysSelect,
-});
-
-export const Option = createComponent({
-	react: React,
-	tagName: "nys-option",
-	elementClass: NysOption,
-});
-
-export const Alert = createComponent({
+export const NysAlertComponent = createComponent({
 	react: React,
 	tagName: "nys-alert",
 	elementClass: NysAlert,
 });
 
-export const Button = createComponent({
+export const NysButtonComponent = createComponent({
 	react: React,
 	tagName: "nys-button",
 	elementClass: NysButton,
 });
-export const Toggle = createComponent({
+
+export const NysCheckboxComponent = createComponent({
+	react: React,
+	tagName: "nys-checkbox",
+	elementClass: NysCheckbox,
+});
+
+export const NysCheckboxgroupComponent = createComponent({
+	react: React,
+	tagName: "nys-checkboxgroup",
+	elementClass: NysCheckboxgroup,
+});
+
+export const NysGlobalFooterComponent = createComponent({
+	react: React,
+	tagName: "nys-globalfooter",
+	elementClass: NysGlobalFooter,
+});
+
+export const NysGlobalHeaderComponent = createComponent({
+	react: React,
+	tagName: "nys-globalheader",
+	elementClass: NysGlobalHeader,
+});
+
+export const NysOptionComponent = createComponent({
+	react: React,
+	tagName: "nys-option",
+	elementClass: NysOption,
+});
+
+export const NysRadiobuttonComponent = createComponent({
+	react: React,
+	tagName: "nys-radiobutton",
+	elementClass: NysRadiobutton,
+});
+
+export const NysRadioGroupComponent = createComponent({
+	react: React,
+	tagName: "nys-radiogroup",
+	elementClass: NysRadiogroup,
+});
+
+export const NysSelectComponent = createComponent({
+	react: React,
+	tagName: "nys-select",
+	elementClass: NysSelect,
+});
+
+export const NysTextInputComponent = createComponent({
+	react: React,
+	tagName: "nys-textinput",
+	elementClass: NysTextinput,
+});
+
+export const NysTextareaComponent = createComponent({
+	react: React,
+	tagName: "nys-textarea",
+	elementClass: NysTextarea,
+});
+
+export const NysToggleComponent = createComponent({
 	react: React,
 	tagName: "nys-toggle",
 	elementClass: NysToggle,
 });
+
+export const NysUnavFooterComponent = createComponent({
+	react: React,
+	tagName: "nys-unavfooter",
+	elementClass: NysUnavFooter,
+});
+
+export const NysUnavHeaderComponent = createComponent({
+	react: React,
+	tagName: "nys-unavheader",
+	elementClass: NysUnavHeader,
+});
+
