f436a984635c24b1046e1be64dada1c2584bbc9f
NovaCat35 <bergintonatcapital@gmail.com>
2026-02-18 15:43:46 -0500

    Update components in util and cleanup main branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,20 @@
 import { Outlet } from "react-router-dom";
-// import {
-//   NysSkipnavComponent,
-//   NysGlobalHeaderComponent,
-//   NysGlobalFooterComponent,
-//   NysUnavFooterComponent,
-//   NysUnavHeaderComponent,
-//   NysBacktotopComponent,
-// } from "./utils/nysds-components";
+import {
+  NysSkipnavComponent,
+  NysGlobalHeaderComponent,
+  NysGlobalFooterComponent,
+  NysUnavFooterComponent,
+  NysUnavHeaderComponent,
+  NysBacktotopComponent,
+} from "./utils/nysds-components";
 import "./styles/App.css";
 
 function App() {
   return (
     <>
-      {/* <NysSkipnavComponent />
-      <NysUnavHeaderComponent /> */}
-      {/* <NysGlobalHeaderComponent
-        appName="React Demo Form"
-        agencyName="Office of Information Technology Services"
-        homepageLink="/nysds-react-demo"
-      >
-        <ul>
-          <li>
-            <a href="/nysds-react-demo">Home</a>
-          </li>
-          <li>
-            <button type="button">Form Examples</button>
-            <ul>
-              <li><a href="/nysds-react-demo/#/basic-form">Basic Form</a></li>
-              <li><a href="/nysds-react-demo/#/stepper">Stepper</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="/nysds-react-demo/#/about">About</a>
-          </li>
-        </ul>
-      </NysGlobalHeaderComponent> */}
-      {/* <NysGlobalHeaderComponent
+      <NysSkipnavComponent />
+      <NysUnavHeaderComponent />
+      <NysGlobalHeaderComponent
         appName="React Demo Form"
         agencyName="Office of Information Technology Services"
         homepageLink="/"
@@ -54,11 +33,11 @@ function App() {
             <a href="/nysds-react-demo/#/about">About</a>
           </li>
         </ul>
-      </NysGlobalHeaderComponent> */}
+      </NysGlobalHeaderComponent>
       <main className="nys-flex-1 nys-display-flex nys-flex-column">
         <Outlet />
       </main>
-      {/* <NysGlobalFooterComponent agencyName="Office of Information Technology Services">
+      <NysGlobalFooterComponent agencyName="Office of Information Technology Services">
         <ul>
           <li>
             <a href="https://its.ny.gov">ITS Home</a>
@@ -70,7 +49,7 @@ function App() {
       </NysGlobalFooterComponent>
 
       <NysUnavFooterComponent />
-      <NysBacktotopComponent /> */}
+      <NysBacktotopComponent /> 
     </>
   );
 }
